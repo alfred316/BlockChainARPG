8ec5ce97bc79453e509cbd4694d51ae5a3d03845
Alfred Shaker <ashaker@kent.edu>
2020-10-19 20:11:09 -0400

    add menu scene
    added menu test scene with two buttons that call public functions from CryptoManager

---
+++ Assets/Scripts/Crypto/CryptoManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CryptoManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //for button1
+    public void ButtonOneClick()
+    {
+        Debug.Log("button 1 pressed");
+    }
+
+    //for button2
+    public void ButtonTwoClick()
+    {
+        Debug.Log("button 2 pressed");
+    }
+}
