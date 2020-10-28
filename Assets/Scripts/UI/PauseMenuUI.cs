ff955a25d97871c1d4e788d121ab83172f731f9f
Alfred Shaker <ashaker@kent.edu>
2020-10-27 22:10:22 -0400

    adding pause scree and trying to fix bugs

---
+++ Assets/Scripts/UI/PauseMenuUI.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenuUI : MonoBehaviour
+{
+    public GameObject PauseUI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (PauseUI.activeInHierarchy)
+            {
+                PauseUI.SetActive(false);
+            }
+            else if (!PauseUI.activeInHierarchy)
+            {
+                PauseUI.SetActive(true);
+            }
+        }
+    }
+}
